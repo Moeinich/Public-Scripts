0a7b7e0fefcc47c61f18f167055f7c566094ff92
Moeinich <danielbang35@gmail.com>
2024-04-22 17:54:39 +0200

    Update PublicAlcher.java

--- PublicAlcher/src/main/java/main/PublicAlcher.java
+++ PublicAlcher/src/main/java/main/PublicAlcher.java
@@ -17,8 +17,8 @@ import static helpers.Interfaces.*;
 
 
 @ScriptManifest(
-        name = "PublicAlcher",
-        description = "An easy to use Alcher",
+        name = "Public Alcher",
+        description = "An easy to use Alcher. Feel free to contribute: https://github.com/Moeinich/Public-Scripts",
         version = "1.0",
         guideLink = "",
         categories = {ScriptCategory.Magic}
