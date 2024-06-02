16d4a28b22183a98ec0419244beb6bd5c7df4ddd
Shadiezz <danielbang35@gmail.com>
2024-06-02 10:39:07 +0200

    small upt

--- PublicMiner/src/main/java/tasks/performMining.java
+++ PublicMiner/src/main/java/tasks/performMining.java
@@ -28,7 +28,7 @@ public class performMining extends Task {
         if (!Player.tileEquals(locationInfo.getStepLocation(), location)) {
             Logger.log("Stepping to vein spot");
             Walker.step(locationInfo.getStepLocation(), regionInfo.getWorldRegion());
-            Condition.wait(() -> Player.atTile(locationInfo.getStepLocation(), regionInfo.getWorldRegion()), 200, 30);
+            Condition.wait(() -> Player.atTile(locationInfo.getStepLocation(), regionInfo.getWorldRegion()), 200, 10);
             return true;
         }
 
