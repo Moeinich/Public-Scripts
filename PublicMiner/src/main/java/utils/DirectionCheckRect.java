04ca1fde27d733e9e4926d30acd4b01bcdf5551c
Moeinich <danielbang35@gmail.com>
2024-11-22 11:33:20 +0100

    north check

--- PublicMiner/src/main/java/utils/DirectionCheckRect.java
+++ PublicMiner/src/main/java/utils/DirectionCheckRect.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public enum DirectionCheckRect {
     WEST(new Rectangle(385, 261, 25, 31)),
-    NORTH(new Rectangle(431, 237, 22, 18)),
+    NORTH(new Rectangle(433, 225, 20, 14)),
     EAST(new Rectangle(478, 265, 30, 25)),
     SOUTH(new Rectangle(438, 300, 27, 35));
 
