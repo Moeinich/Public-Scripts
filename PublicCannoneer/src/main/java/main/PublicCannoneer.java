7d9af5fc8cf6981c795923e67204d7bcf5792a4d
Moeinich <danielbang35@gmail.com>
2024-12-10 10:19:07 +0100

    1.16 public Canonneer

--- PublicCannoneer/src/main/java/main/PublicCannoneer.java
+++ PublicCannoneer/src/main/java/main/PublicCannoneer.java
@@ -4,6 +4,7 @@ import helpers.*;
 import helpers.annotations.ScriptConfiguration;
 import helpers.annotations.ScriptManifest;
 import helpers.utils.OptionType;
+import tasks.BreakManager;
 import tasks.CheckForItems;
 import tasks.PerformCannoning;
 import utils.Task;
@@ -11,6 +12,7 @@ import utils.Task;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import static helpers.Interfaces.*;
 
@@ -18,7 +20,7 @@ import static helpers.Interfaces.*;
 @ScriptManifest(
         name = "Public Cannoneer",
         description = "An easy to use AFK cannon script. Feel free to contribute: https://github.com/Moeinich/Public-Scripts",
-        version = "1.15",
+        version = "1.16",
         guideLink = "https://wiki.mufasaclient.com/docs/public-cannoneer/",
         categories = {ScriptCategory.Combat}
 )
@@ -30,21 +32,36 @@ import static helpers.Interfaces.*;
                         defaultValue = "31",
                         minMaxIntValues = {15, 40},
                         optionType = OptionType.INTEGER_SLIDER
+                ),
+                @ScriptConfiguration(
+                        name = "Break every",
+                        description = "Break every random x minutes",
+                        defaultValue = "31",
+                        minMaxIntValues = {15, 40},
+                        optionType = OptionType.INTEGER_SLIDER
                 )
         }
 )
 
 public class PublicCannoneer extends AbstractScript {
     public static int waitTime;
+    public static int breakEvery;
+
     @Override
     public void onStart(){
         Logger.log("Setting everything up");
         Map<String, String> configs = getConfigurations();
         waitTime = Integer.parseInt(configs.get("Max wait time"));
+        breakEvery = Integer.parseInt(configs.get("Break every"));
+
+        // Disable break/afk handlers to use its own built-in
+        Client.disableBreakHandler();
+        Client.disableAFKHandler();
     }
 
     // Task list!
     List<Task> cannonTasks = Arrays.asList(
+            new BreakManager(),
             new CheckForItems(),
             new PerformCannoning()
     );
@@ -58,5 +75,16 @@ public class PublicCannoneer extends AbstractScript {
                 return;
             }
         }
+    }
+
+    static Random random = new Random();
+    public static int generateRandomDelay(int lowerBound, int upperBound) {
+        // Swap if lowerBound is greater than upperBound
+        if (lowerBound > upperBound) {
+            int temp = lowerBound;
+            lowerBound = upperBound;
+            upperBound = temp;
+        }
+        return lowerBound + random.nextInt(upperBound - lowerBound + 1);
     }
 }
---
+++ PublicCannoneer/src/main/java/tasks/BreakManager.java
@@ -0,0 +1,60 @@
+package tasks;
+
+import utils.Task;
+
+import java.util.Random;
+
+import static helpers.Interfaces.*;
+import static main.PublicCannoneer.breakEvery;
+import static main.PublicCannoneer.generateRandomDelay;
+
+public class BreakManager extends Task {
+    public static int shouldBreakAt = 0;
+    public static boolean shouldBreakNow = false;
+    private static final Random random = new Random(System.nanoTime());
+
+    private static void generateRandomNextBreakCount() {
+        int variability = random.nextInt(11) - 5;  // Range [-5, 5]
+        shouldBreakAt = breakEvery + variability;
+        Logger.log("Next break scheduled after " + shouldBreakAt + " minutes.");
+    }
+
+    @Override
+    public boolean activate() {
+        if (shouldBreakAt == 0) {
+            generateRandomNextBreakCount();
+        }
+
+        if (Player.leveledUp()) {
+            Client.sendKeystroke("KEYCODE_SPACE");
+            Condition.sleep(generateRandomDelay(1000, 2000));
+        }
+
+
+        return shouldBreakNow;
+    }
+
+    @Override
+    public boolean execute() {
+        int breakMinutes = random.nextInt(4) + 1;  // Generates a number from 1 to 4
+        int breakSeconds = random.nextInt(60);     // Use the same random instance here as well
+        int breakMillis = breakMinutes * 60000 + breakSeconds * 1000;  // Convert minutes and seconds to milliseconds
+
+        // Log the precise break duration
+        Logger.log("Taking a break for " + breakMinutes + " minute(s) and " + breakSeconds + " second(s).");
+        Paint.setStatus("Taking a break for 0" + breakMinutes + ":" + breakSeconds);
+
+        // Breaking logic
+        Logout.logout();
+        Condition.sleep(breakMillis);  // Sleep for the calculated duration
+
+        Logger.log("Break over, resuming script.");
+        Paint.setStatus("Break over, resuming script");
+        Paint.setStatus("Logging in");
+        Login.login();
+
+        generateRandomNextBreakCount();
+        shouldBreakNow = false;
+        return true;
+    }
+}
